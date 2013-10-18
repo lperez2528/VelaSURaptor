cf0dcfe1309b279a839101aafa9afd72d96ebb19
Matt Malone <mattagape@gmail.com>
2013-10-18 17:04:27 -0500

    updated submission.rb and survey.rb

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,5 @@
 class Submission < ActiveRecord::Base
   belongs_to :survey
   belongs_to :taker, class_name: "User"
+  has_many :responses
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,6 @@
 class Survey < ActiveRecord::Base
   has_many :questions
   belongs_to :creator, class_name: "User"
+  has_many :submissions
+  has_many :takers, through: :submissions
 end
