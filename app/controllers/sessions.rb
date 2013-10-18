479bffc7c208532a7712f2a00ed8d4fdfd723a5d
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-18 14:54:01 -0500

    housekeeping

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -27,3 +27,26 @@ post '/signup' do
   session[:user_id] = user.id
   redirect '/'
 end
+
+get '/edit_profile' do
+  @user = current_user
+  erb :edit_profile
+end
+
+post '/edit_profile' do
+  @user = current_user
+  @user.update_attributes(first_name: params[:first_name],
+                          last_name: params[:last_name],
+                          email: params[:email],
+                          birthdate: params[:birthdate],
+                          home_city: params[:home_city],
+                          zipcode: params[:zipcode],
+                          gravatar: params[:gravatar])
+  redirect to('/logged_homepage')
+end
+
+get '/logged_homepage' do
+  @user = current_user
+  @surveys = Survey.all
+  erb :logged_homepage
+end
