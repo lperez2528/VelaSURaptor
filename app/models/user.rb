0a8ec3f1b1fc0de2f9d09d6f4c4bf8d0531bfc06
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-18 19:24:35 -0500

    user profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   attr_protected :password_digest
+
+  PUBLIC_ATTRIBUTES = ["first_name", "last_name", "email", "home_city", "birthdate"]
   
   has_many :created_surveys, class_name:"Survey", foreign_key: :creator_id  
   has_many :submissions, foreign_key: :taker_id
@@ -8,6 +10,12 @@ class User < ActiveRecord::Base
 
   def name
     "#{first_name} #{last_name}"
+  end
+
+  def available_attributes
+    attributes.select do |k,v|
+      PUBLIC_ATTRIBUTES.include?(k) && v
+    end
   end
 
 end
