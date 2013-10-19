5f1f4cf3d40bd103077f400e9096c2334f543f39
Matt Malone <mattagape@gmail.com>
2013-10-19 14:40:47 -0500

    created responses for taker

--- app/controllers/survey_page_controller.rb
+++ app/controllers/survey_page_controller.rb
@@ -8,10 +8,20 @@ end
 post '/survey_page' do
   @user = User.find(1)
   @survey = Survey.find(1)
-  submissions = Submission.find(1)
   @questions = @survey.questions
+  @submission = Submission.create
+
+  @questions.each do |question|
+    question.choices.each do |choice|
+      p choice.responses << Response.create(submission_id: @submission.id, choice_id: choice.id)
+    end
+  end
+
+
+
   # @choices = @questions.choices
   @user.taken_surveys << @survey
+
   
   p params
 
