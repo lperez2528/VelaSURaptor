8258fc7c90c7b8984e0eb25f00e3f1605d1a90e1
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-18 19:50:35 -0500

    create surveys

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -2,3 +2,27 @@ get '/' do
   @surveys = Survey.all
   erb :index
 end
+
+get '/create_survey' do 
+  erb :create_survey
+end
+ 
+ 
+post '/create_survey' do
+  survey = Survey.create(title: params[:create_title])
+  questions = Hash[*params[:mc_question].flatten]
+ 
+  questions.each do |k,v|
+    choice_array = v['mc_answer']
+ 
+    question = Question.create(description: k)
+    survey.questions << question
+ 
+    choice_array.each do |choice| 
+      question.choices << Choice.create(text: choice) 
+    end
+  end
+ 
+  redirect('/')
+ 
+end
