c74ebe3a3547a1933ef0baf5de84ca59dca6a714
Matt Malone <mattagape@gmail.com>
2013-10-18 20:39:11 -0500

    iterating through questions/choices to print them out

--- app/controllers/survey_page_controller.rb
+++ app/controllers/survey_page_controller.rb
@@ -6,10 +6,16 @@ end
 
 
 post '/survey_page' do
+  @user = User.find(1)
   @survey = Survey.find(1)
+  submissions = Submission.find(1)
   @questions = @survey.questions
-  @choices = @questions.choices
+  # @choices = @questions.choices
+  @user.taken_surveys << @survey
   
+  p params
 
+  # @user.submissions.find(1).responses << Response.create(submission_id: submission, choice_id: )
+  
   redirect '/'
 end
