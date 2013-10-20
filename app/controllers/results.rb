ed9a4918b4504bbd7dfab7d650a5456a5a9d2ac5
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-20 14:02:53 -0500

    bug fixed: if survey hasn't been taken yet

--- app/controllers/results.rb
+++ app/controllers/results.rb
@@ -1,10 +1,11 @@
-get "/results/survey/:survey_id" do
+get "/results/survey_page/:survey_id" do
 
 @survey = Survey.find(params[:survey_id])
 @user = User.find(session[:user_id])
 @total_takers = @survey.takers.count
 
+  
+    erb :results_page
+  
 
-
-erb :results_page
 end
